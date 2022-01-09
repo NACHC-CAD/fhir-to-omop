3dd72df713062cc4e21f847d0d91145252ce0b7b
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-01-09 10:13:10 -0500

    Added patient fetcher

---
+++ src/main/java/org/nachc/tools/fhirtoomop/util/synthea/SyntheaPatientSummaryListFetcher.java
@@ -0,0 +1,45 @@
+package org.nachc.tools.fhirtoomop.util.synthea;
+
+import org.nachc.tools.fhirtoomop.util.synthea.properties.SyntheaParams;
+
+import com.nach.core.util.http.HttpRequestClient;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * This class implements the ability to fetch a list of patient summaries from
+ * the synthia fhir server.
+ *
+ */
+
+@Slf4j
+public class SyntheaPatientSummaryListFetcher {
+
+	private HttpRequestClient client;
+
+	public HttpRequestClient getClient() {
+		return this.client;
+	}
+
+	public int getStatusCode() {
+		return this.client.getStatusCode();
+	}
+
+	public String fetchPatients(int howMany) {
+		String url = SyntheaParams.getUrl();
+		String key = SyntheaParams.getKey();
+		url += "/Patient?";
+		url += "_count=" + howMany;
+		url += "&apikey=" + key;
+		log.info("URL: " + url);
+		this.client = new HttpRequestClient(url);
+		client.doGet();
+		int status = client.getStatusCode();
+		log.info("Got status: " + status);
+		String response = client.getResponse();
+		log.info("Response length: " + response.length());
+		return response;
+	}
+
+}
