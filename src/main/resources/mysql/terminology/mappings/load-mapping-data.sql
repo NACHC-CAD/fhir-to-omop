57e3e818fe48c4c4b2204ed88a079c3c0fcdcbe6
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-01-15 09:30:18 -0500

    more on terminology

--- src/main/resources/mysql/terminology/mappings/load-mapping-data.sql
+++ src/main/resources/mysql/terminology/mappings/load-mapping-data.sql
@@ -5,19 +5,22 @@ SET GLOBAL local_infile=1;
 load data local infile 'C:\\test\\fhir-to-omop\\RaceAndEthnicityCDC-OMOP-MAPPING-Race.csv'
 into table fhir_to_omop_race
 fields terminated by ','
-enclosed by '"'
-lines terminated by '\n'
+optionally enclosed by '"'
+lines terminated by '\r\n'
 ignore 1 lines
 ;
 
 load data local infile 'C:\\test\\fhir-to-omop\\RaceAndEthnicityCDC-OMOP-MAPPING-Eth.csv'
 into table fhir_to_omop_ethnicity
 fields terminated by ','
-enclosed by '"'
-lines terminated by '\n'
+optionally enclosed by '"'
+lines terminated by '\r\n'
 ignore 1 lines
 ;
 
+select * from fhir_to_omop_race;
+
+select count(*) from fhir_to_omop_race;
 
 
 
