7f534b5b0e6c4ece5a4abe3e906dd42a0c864543
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-01-16 22:08:42 -0500

    fixes for terminology upload to mysql

--- src/main/resources/mysql/load-terminology.sql
+++ src/main/resources/mysql/load-terminology.sql
@@ -13,7 +13,7 @@ show variables like 'net_read_timeout';
 load data local infile 'C:\\_WORKSPACES\\nachc\\_PROJECT\\cosmos\\omop-concepts\\vocabulary_download_v5_{9259d46c-23e7-4760-b8d6-cddd5d86be7d}_1631306538104\\DOMAIN.csv'
 into table domain
 fields terminated by '\t'
-optionally enclosed by '"'
+-- optionally enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines
 ;
@@ -21,7 +21,7 @@ ignore 1 lines
 load data local infile 'C:\\_WORKSPACES\\nachc\\_PROJECT\\cosmos\\omop-concepts\\vocabulary_download_v5_{9259d46c-23e7-4760-b8d6-cddd5d86be7d}_1631306538104\\CONCEPT.csv'
 into table concept
 fields terminated by '\t'
-optionally enclosed by '"'
+-- optionally enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines
 ;
@@ -29,7 +29,7 @@ ignore 1 lines
 load data local infile 'C:\\_WORKSPACES\\nachc\\_PROJECT\\cosmos\\omop-concepts\\vocabulary_download_v5_{9259d46c-23e7-4760-b8d6-cddd5d86be7d}_1631306538104\\CONCEPT_RELATIONSHIP.csv'
 into table concept_relationship
 fields terminated by '\t'
-optionally enclosed by '"'
+-- optionally enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines
 ;
@@ -37,7 +37,7 @@ ignore 1 lines
 load data local infile 'C:\\_WORKSPACES\\nachc\\_PROJECT\\cosmos\\omop-concepts\\vocabulary_download_v5_{9259d46c-23e7-4760-b8d6-cddd5d86be7d}_1631306538104\\CONCEPT_ANCESTOR.csv'
 into table concept_ancestor
 fields terminated by '\t'
-optionally enclosed by '"'
+-- optionally enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines
 ;
@@ -45,7 +45,7 @@ ignore 1 lines
 load data local infile 'C:\\_WORKSPACES\\nachc\\_PROJECT\\cosmos\\omop-concepts\\vocabulary_download_v5_{9259d46c-23e7-4760-b8d6-cddd5d86be7d}_1631306538104\\CONCEPT_SYNONYM.csv'
 into table concept_synonym
 fields terminated by '\t'
-optionally enclosed by '"'
+-- optionally enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines
 ;
@@ -53,7 +53,7 @@ ignore 1 lines
 load data local infile 'C:\\_WORKSPACES\\nachc\\_PROJECT\\cosmos\\omop-concepts\\vocabulary_download_v5_{9259d46c-23e7-4760-b8d6-cddd5d86be7d}_1631306538104\\DRUG_STRENGTH.csv'
 into table drug_strength
 fields terminated by '\t'
-optionally enclosed by '"'
+-- optionally enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines
 ;
@@ -62,7 +62,7 @@ ignore 1 lines
 load data local infile 'C:\\_WORKSPACES\\nachc\\_PROJECT\\cosmos\\omop-concepts\\vocabulary_download_v5_{9259d46c-23e7-4760-b8d6-cddd5d86be7d}_1631306538104\\CONCEPT_CPT4.csv'
 into table concept_cpt4
 fields terminated by '\t'
-optionally enclosed by '"'
+-- optionally enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines
 ;
@@ -71,7 +71,7 @@ ignore 1 lines
 load data local infile 'C:\\_WORKSPACES\\nachc\\_PROJECT\\cosmos\\omop-concepts\\vocabulary_download_v5_{9259d46c-23e7-4760-b8d6-cddd5d86be7d}_1631306538104\\RELATIONSHIP.csv'
 into table relationship
 fields terminated by '\t'
-optionally enclosed by '"'
+-- optionally enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines
 ;
@@ -79,7 +79,7 @@ ignore 1 lines
 load data local infile 'C:\\_WORKSPACES\\nachc\\_PROJECT\\cosmos\\omop-concepts\\vocabulary_download_v5_{9259d46c-23e7-4760-b8d6-cddd5d86be7d}_1631306538104\\CONCEPT_CLASS.csv'
 into table concept_class
 fields terminated by '\t'
-optionally enclosed by '"'
+-- optionally enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines
 ;
@@ -87,7 +87,7 @@ ignore 1 lines
 load data local infile 'C:\\_WORKSPACES\\nachc\\_PROJECT\\cosmos\\omop-concepts\\vocabulary_download_v5_{9259d46c-23e7-4760-b8d6-cddd5d86be7d}_1631306538104\\VOCABULARY.csv'
 into table vocabulary
 fields terminated by '\t'
-optionally enclosed by '"'
+-- optionally enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines
 ;
