2a9458815c0eb33c0db290e1a5690ba656b80375
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-01-24 13:45:36 -0500

    more on observation

--- src/main/java/org/nachc/tools/fhirtoomop/util/fhir/parser/observation/ObservationParser.java
+++ src/main/java/org/nachc/tools/fhirtoomop/util/fhir/parser/observation/ObservationParser.java
@@ -2,6 +2,7 @@ package org.nachc.tools.fhirtoomop.util.fhir.parser.observation;
 
 import org.hl7.fhir.dstu3.model.Coding;
 import org.hl7.fhir.dstu3.model.Observation;
+import org.hl7.fhir.dstu3.model.Quantity;
 import org.nachc.tools.fhirtoomop.util.fhir.general.FhirUtil;
 
 import com.ibm.icu.util.StringTokenizer;
@@ -29,27 +30,52 @@ public class ObservationParser {
 	public String getEncounterId() {
 		try {
 			String str = this.obs.getContext().getReference();
-			if(str.indexOf("/") > 0) {
+			if (str.indexOf("/") > 0) {
 				StringTokenizer tokenizer = new StringTokenizer(str, "/");
 				String type = tokenizer.nextToken();
 				String val = tokenizer.nextToken();
-				if("encounter".equalsIgnoreCase(type) == false) {
+				if ("encounter".equalsIgnoreCase(type) == false) {
 					return null;
 				} else {
 					return val;
 				}
 			}
 			return str;
-		} catch(Exception exp) {
+		} catch (Exception exp) {
 			return null;
 		}
 	}
-	
+
 	public Coding getCategory() {
 		try {
-			// TODO: (JEG) Just getting first here (might need to be expanded for other use cases)
+			// TODO: (JEG) Just getting first here (might need to be expanded for other use
+			// cases)
 			return this.obs.getCategory().get(0).getCodingFirstRep();
-		} catch(Exception exp) {
+		} catch (Exception exp) {
+			return null;
+		}
+	}
+
+	public Coding getCode() {
+		try {
+			return this.obs.getCode().getCodingFirstRep();
+		} catch (Exception exp) {
+			return null;
+		}
+	}
+
+	public Coding getValueCoding() {
+		try {
+			return this.obs.getValueCodeableConcept().getCodingFirstRep();
+		} catch (Exception exp) {
+			return null;
+		}
+	}
+
+	public Quantity getValueQuantity() {
+		try {
+			return obs.getValueQuantity();
+		} catch (Exception exp) {
 			return null;
 		}
 	}
