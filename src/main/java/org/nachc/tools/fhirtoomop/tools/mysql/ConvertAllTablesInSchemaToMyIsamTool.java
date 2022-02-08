63126afc27420df55a38fe6b59d1425dd2c09c59
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-02-08 14:02:14 -0500

    added stuff for MyISAM

--- src/main/java/org/nachc/tools/fhirtoomop/tools/mysql/ConvertAllTablesInSchemaToMyIsamTool.java
+++ src/main/java/org/nachc/tools/fhirtoomop/tools/mysql/ConvertAllTablesInSchemaToMyIsamTool.java
@@ -6,17 +6,22 @@ import org.nachc.tools.fhirtoomop.util.db.mysql.MySqlDatabaseConnectionFactory;
 import org.nachc.tools.fhirtoomop.util.db.mysql.util.ConvertAllTablesToMyIsam;
 import org.yaorma.database.Database;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ConvertAllTablesInSchemaToMyIsamTool {
 
 	private static final String SCHEMA = "synthe_omop";
 	
-	public static void exec() {
+	public static void main(String[] args) {
+		log.info("CONVERTING TABLES TO MYISAM")
 		Connection conn = MySqlDatabaseConnectionFactory.getSyntheaConnection();
 		try {
 			new ConvertAllTablesToMyIsam().exec(SCHEMA, conn);
 		} finally {
 			Database.close(conn);
 		}
+		log.info("Done.");
 	}
 
 }
