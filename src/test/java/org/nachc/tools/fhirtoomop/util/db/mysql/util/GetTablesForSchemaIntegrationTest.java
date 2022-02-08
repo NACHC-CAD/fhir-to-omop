47669e17c33acfa4e9a65d67a1134b38131087c0
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-02-08 13:43:43 -0500

    database utils for optimizations

---
+++ src/test/java/org/nachc/tools/fhirtoomop/util/db/mysql/util/GetTablesForSchemaIntegrationTest.java
@@ -0,0 +1,34 @@
+package org.nachc.tools.fhirtoomop.util.db.mysql.util;
+
+import java.sql.Connection;
+
+import org.junit.Test;
+import org.nachc.tools.fhirtoomop.util.db.mysql.MySqlDatabaseConnectionFactory;
+import org.yaorma.database.Data;
+import org.yaorma.database.Database;
+import org.yaorma.database.Row;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class GetTablesForSchemaIntegrationTest {
+
+	@Test
+	public void shouldGetTables() {
+		log.info("Starting test...");
+		Connection conn = MySqlDatabaseConnectionFactory.getSyntheaConnection();
+		try {
+			log.info("Getting tables...");
+			String schemaName = "information_schema";
+			Data data = GetTablesForSchema.exec(schemaName, conn);
+			log.info("Got " + data.size() + " records.");
+			for(Row row : data) {
+				log.info("\t" + row.get("tableSchema") + "." + row.get("tableName"));
+			}
+		} finally {
+			Database.close(conn);
+		}
+		log.info("Done.");
+	}
+	
+}
