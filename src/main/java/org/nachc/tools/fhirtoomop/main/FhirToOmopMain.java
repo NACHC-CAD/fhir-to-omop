334b5be6519cd2872308ed9600dfcabaa3bfe652
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-03-09 15:49:27 -0500

    working on exe jar, this version is almost working

---
+++ src/main/java/org/nachc/tools/fhirtoomop/main/FhirToOmopMain.java
@@ -0,0 +1,21 @@
+package org.nachc.tools.fhirtoomop.main;
+
+import com.nach.core.util.file.FileUtil;
+
+public class FhirToOmopMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to fhir-to-omop");
+		if (args == null || args.length == 0) {
+			printZeroArgsMsg();
+		} else {
+
+		}
+	}
+
+	private static void printZeroArgsMsg() {
+		String msg = FileUtil.getAsString("/org/nachc/tools/fhirtoomop/main/msg/zero-args-error.txt");
+		System.out.println(msg);
+	}
+
+}
