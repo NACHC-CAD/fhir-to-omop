51978345914497e7dd5da1c8b7788af341d6908b
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-03-15 05:02:10 -0400

    ABOUT TO SWITCH AWAY FROM RCaller

--- src/main/java/org/nachc/tools/fhirtoomop/tools/dqd/RunDqdInitScript.java
+++ src/main/java/org/nachc/tools/fhirtoomop/tools/dqd/RunDqdInitScript.java
@@ -1,5 +1,7 @@
 package org.nachc.tools.fhirtoomop.tools.dqd;
 
+import java.io.OutputStream;
+
 import com.github.rcaller.rstuff.RCaller;
 import com.github.rcaller.rstuff.RCode;
 import com.nach.core.util.file.FileUtil;
@@ -9,14 +11,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RunDqdInitScript {
 
-	private static final String PATH = "/dqd/init-params-for-dtq.r";
+	private static final String PATH = "/dqd/run-dqd.r";
+//	private static final String PATH = "/dqd/init-params-for-dtq.r";
 
 	public static void exec() {
 		String script = FileUtil.getAsString(PATH);
 		log.info("SCRIPT:\n\n" + script + "\n\n");
 		log.info("RUNNING SCRIPT...\n");
 		RCaller caller = RCaller.create();
-		// caller.redirectROutputToStream(null);
+		caller.redirectROutputToStream(System.out);
+		
 		StringBuffer sb = new StringBuffer();
 		sb.append(script);
 		RCode code = RCode.create(sb);
