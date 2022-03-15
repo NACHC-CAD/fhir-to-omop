a24b637e37d43bf226ca16a1c418b6a398b084da
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-03-15 05:29:56 -0400

    STILL USING RCALLER, MAIN METHOD IN RunDqdInitScript IS WORKING

--- src/main/java/org/nachc/tools/fhirtoomop/tools/dqd/RunDqdInitScript.java
+++ src/main/java/org/nachc/tools/fhirtoomop/tools/dqd/RunDqdInitScript.java
@@ -1,8 +1,10 @@
 package org.nachc.tools.fhirtoomop.tools.dqd;
 
-import java.io.OutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 import com.github.rcaller.rstuff.RCaller;
+import com.github.rcaller.rstuff.RCallerOptions;
 import com.github.rcaller.rstuff.RCode;
 import com.nach.core.util.file.FileUtil;
 
@@ -11,8 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RunDqdInitScript {
 
-	private static final String PATH = "/dqd/run-dqd.r";
-//	private static final String PATH = "/dqd/init-params-for-dtq.r";
+//	private static final String PATH = "/dqd/run-dqd.r";
+	private static final String PATH = "/dqd/init-params-for-dtq.r";
 
 	public static void exec() {
 		String script = FileUtil.getAsString(PATH);
@@ -20,13 +22,27 @@ public class RunDqdInitScript {
 		log.info("RUNNING SCRIPT...\n");
 		RCaller caller = RCaller.create();
 		caller.redirectROutputToStream(System.out);
-		
+
 		StringBuffer sb = new StringBuffer();
 		sb.append(script);
 		RCode code = RCode.create(sb);
 		caller.setRCode(code);
 		caller.runOnly();
 		log.info("Done.");
+	}
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		int[] values = {1,2,3,4,5,6};
+		String fileContent = FileUtil.getAsString(PATH);
+		RCode code = RCode.create();
+		code.addRCode(fileContent);
+		code.addIntArray("input", values);
+//		code.addRCode("result <- customMean(input)");
+		RCaller caller = RCaller.create(code, RCallerOptions.create());
+		caller.redirectROutputToStream(System.out);
+		caller.runAndReturnResult("dbms");
+		String foo = caller.getParser().getAsStringArray("dbms")[0];
+		System.out.println(foo);
 	}
 
 }
