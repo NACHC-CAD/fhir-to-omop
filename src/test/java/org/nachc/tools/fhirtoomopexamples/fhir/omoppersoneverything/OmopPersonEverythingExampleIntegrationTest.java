1a99cb90f97ec6eca477be0945e7e86b722a77ae
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-03-17 20:32:18 -0400

    working on documentation

---
+++ src/test/java/org/nachc/tools/fhirtoomopexamples/fhir/omoppersoneverything/OmopPersonEverythingExampleIntegrationTest.java
@@ -0,0 +1,15 @@
+package org.nachc.tools.fhirtoomopexamples.fhir.omoppersoneverything;
+
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class OmopPersonEverythingExampleIntegrationTest {
+
+	@Test
+	public void shouldCreateOmopPersonEverything() {
+		
+	}
+	
+}
