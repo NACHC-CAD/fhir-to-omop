c732767e89bde164305e9e63ac984ae88ce12932
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-03-30 06:12:28 -0400

    REFACTORING

---
+++ src/test/java/org/nachc/tools/fhirtoomop/omop/write/singlepatient/WriteOmopPersonToDatabaseIntegrationTest.java
@@ -0,0 +1,51 @@
+package org.nachc.tools.fhirtoomop.omop.write.singlepatient;
+
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+import java.util.List;
+
+import org.junit.Test;
+import org.nachc.tools.fhirtoomop.fhir.patient.FhirPatient;
+import org.nachc.tools.fhirtoomop.fhir.patient.factory.FhirPatientFactory;
+import org.nachc.tools.fhirtoomop.omop.person.OmopPerson;
+import org.nachc.tools.fhirtoomop.omop.person.factory.OmopPersonFactory;
+import org.nachc.tools.fhirtoomop.util.db.connection.OmopDatabaseConnectionFactory;
+import org.nachc.tools.omop.yaorma.dvo.ProcedureOccurrenceDvo;
+import org.yaorma.database.Database;
+
+import com.nach.core.util.file.FileUtil;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class WriteOmopPersonToDatabaseIntegrationTest {
+	
+	private static final String DIR_PATH = "/test/fhir/test-patient-01/5acc8bb4-2d14-4461-a560-228d96459cc3";
+
+	@Test
+	public void shouldWritePatientToDatabase() {
+		log.info("Starting test...");
+		List<String> fileList = FileUtil.listResources(DIR_PATH, getClass());
+		FhirPatient fhirPatient = new FhirPatientFactory(fileList).buildFhirPatient();
+		// get a connection
+		log.info("Getting connection...");
+		Connection conn = OmopDatabaseConnectionFactory.getOmopConnection();
+		log.info("Got connection");
+		try {
+			int before = Database.count("person", conn);
+			log.info("before: " + before);
+			OmopPerson omopPerson = new OmopPersonFactory().build(fhirPatient, conn);
+			WriteOmopPersonToDatabase.exec(omopPerson, conn);
+			Database.commit(conn);
+			int after = Database.count("person", conn);
+			log.info("before: " + before);
+			log.info("after: " + after);
+			assertTrue(after > before);
+		} finally {
+			OmopDatabaseConnectionFactory.close(conn);
+		}
+		log.info("Done.");
+	}
+
+}
--- src/test/java/org/nachc/tools/fhirtoomop/omop/write/threaded/runnable/WriteFhirPatientToOmopRunnableIntegrationTest.java
+++ src/test/java/org/nachc/tools/fhirtoomop/omop/write/threaded/runnable/WriteFhirPatientToOmopRunnableIntegrationTest.java
@@ -1,4 +1,4 @@
-package org.nachc.tools.fhirtoomop.omop.write.singlepatient;
+package org.nachc.tools.fhirtoomop.omop.write.threaded.runnable;
 
 import static org.junit.Assert.assertTrue;
 
@@ -11,7 +11,6 @@ import org.nachc.tools.fhirtoomop.fhir.patient.factory.FhirPatientFactory;
 import org.nachc.tools.fhirtoomop.omop.person.OmopPerson;
 import org.nachc.tools.fhirtoomop.omop.person.factory.OmopPersonFactory;
 import org.nachc.tools.fhirtoomop.util.db.connection.OmopDatabaseConnectionFactory;
-import org.nachc.tools.omop.yaorma.dvo.ProcedureOccurrenceDvo;
 import org.yaorma.database.Database;
 
 import com.nach.core.util.file.FileUtil;
@@ -19,12 +18,12 @@ import com.nach.core.util.file.FileUtil;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class WriteFhirPatientToOmopIntegrationTest {
-	
+public class WriteFhirPatientToOmopRunnableIntegrationTest {
+
 	private static final String DIR_PATH = "/test/fhir/test-patient-01/5acc8bb4-2d14-4461-a560-228d96459cc3";
 
 	@Test
-	public void shouldWritePatientToDatabase() {
+	public void shouldWritePatientUsingRunnable() {
 		log.info("Starting test...");
 		List<String> fileList = FileUtil.listResources(DIR_PATH, getClass());
 		FhirPatient fhirPatient = new FhirPatientFactory(fileList).buildFhirPatient();
@@ -36,11 +35,20 @@ public class WriteFhirPatientToOmopIntegrationTest {
 			int before = Database.count("person", conn);
 			log.info("before: " + before);
 			OmopPerson omopPerson = new OmopPersonFactory().build(fhirPatient, conn);
-			WriteOmopPersonToDatabase.exec(omopPerson, conn);
+			WriteFhirPatientToOmopRunnable runnable = new WriteFhirPatientToOmopRunnable(DIR_PATH, conn, 1);
+			Thread thread = new Thread(runnable);
+			log.info("Starting thread...");
+			thread.start();
+			try {
+				thread.join();
+			} catch (Exception exp) {
+				throw new RuntimeException(exp);
+			}
+			log.info("joined");
 			Database.commit(conn);
 			int after = Database.count("person", conn);
 			log.info("before: " + before);
-			log.info("after: " + after);
+			log.info("after:  " + after);
 			assertTrue(after > before);
 		} finally {
 			OmopDatabaseConnectionFactory.close(conn);
