9ec14cd8c81b23a8493820f268b3b65610e34508
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-03-30 07:18:35 -0400

    REFACTORING

---
+++ src/main/java/org/nachc/tools/fhirtoomop/omop/write/listofpatients/WriteListOfFhirPatientsToOmop.java
@@ -0,0 +1,51 @@
+package org.nachc.tools.fhirtoomop.omop.write.listofpatients;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.nachc.tools.fhirtoomop.omop.write.listofpatients.allatonce.WriteListOfFhirPatientsToOmopAllAtOnce;
+import org.yaorma.database.Database;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * This class should be used to write large lists of FHIR patients to an omop
+ * database.
+ *
+ */
+@Slf4j
+public class WriteListOfFhirPatientsToOmop {
+
+	public static void writeAllFiles(List<String> fileList, List<Connection> connList, int MAX_THREADS) {
+		ArrayList<String> filesToWrite = new ArrayList<String>();
+		int cnt = 0;
+		for (int i = 0; i < fileList.size(); i++) {
+			filesToWrite.add(fileList.get(i));
+			if (i % MAX_THREADS == 0 && i != 0) {
+				new WriteListOfFhirPatientsToOmopAllAtOnce().exec(filesToWrite, connList);
+				for (Connection conn : connList) {
+					Database.commit(conn);
+				}
+				cnt++;
+				filesToWrite = new ArrayList<String>();
+				logMsg(i, cnt);
+			}
+		}
+		if (filesToWrite.size() > 0) {
+			new WriteListOfFhirPatientsToOmopAllAtOnce().exec(filesToWrite, connList);
+		}
+	}
+
+	private static void logMsg(int i, int cnt) {
+		String msg = "";
+		msg += "\n\n\n\n------------------------------------------------";
+		msg += "\nWriting Datafiles";
+		msg += "\nTotal written: " + i;
+		msg += "\nBlock count:   " + cnt;
+		msg += "\n------------------------------------------------\n\n\n";
+		log.info(msg);
+	}
+
+}
