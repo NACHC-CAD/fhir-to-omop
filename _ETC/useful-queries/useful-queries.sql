128c631689eebe042b504364c2296f9b30248f7b
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-04-09 14:27:52 -0400

    fixes for new tests on PROCEDURE_OCCURRENCE

--- _ETC/useful-queries/useful-queries.sql
+++ _ETC/useful-queries/useful-queries.sql
@@ -1,376 +1,277 @@
 use synthea_dev;
+
 use synthea_micro;
 
-select count(*) from concept;
-
-
-select distinct
-	p.person_id,
-	p.person_source_value,
-	con.*
-from 
-	procedure_occurrence po
-	join person p on p.person_id = po.person_id
-	left outer join concept con on con.concept_id = po.procedure_concept_id
-where 1=1
---	and domain_id != 'Procedure'
-order by domain_id
-;
-
-
--- STUFF FOR MEASUREMENT DQD
-
 select count(*) from person;
-
-select * from person;
-
-select * from concept where concept_id = 3020630;
-
-select * from measurement where measurement_concept_id = 3020630;
-
--- concepts failing protein in plasma/blood [g/dL]
-select	
-	p.person_id,
-	p.person_source_value,
-	c.concept_name,
-	m.measurement_id,
-	m.measurement_concept_id,
-	m.value_as_number,
-	uc.concept_name as units
-from
-	measurement m
-	join person p on m.person_id = p.person_id
-	join concept c on m.measurement_concept_id = c.concept_id 
-	join concept uc on m.unit_concept_id = uc.concept_id
-where 1=1
-	and m.measurement_concept_id = 3020630
-	and value_as_number > 15
-order by 
-	p.person_source_value,
-	m.value_as_number desc
-;
-
-
-select * from concept where concept_id = 8713;
-
--- measurement type
-
-select * from measurement;
-
-select distinct 
---	p.person_id,
---	p.person_source_value,
-	c.concept_name,
---	m.measurement_id,
-	m.measurement_concept_id,
---	m.value_as_number,
-	c.concept_name as type,
-	c.domain_id,
-	c.concept_class_id,
-	c.vocabulary_id
-from
-	measurement m
-	join person p on m.person_id = p.person_id
-	join concept c on m.measurement_concept_id = c.concept_id 
-	join concept tc on m.measurement_type_concept_id = tc.concept_id
-where 1=1
-	and tc.concept_id = 0
-order by 
--- 	p.person_source_value,
--- 	m.value_as_number desc
-	c.domain_id,
-	c.concept_class_id,
-	c.vocabulary_id,
-	c.concept_name
-;
-
-select distinct 
-	c.concept_name,
-	m.measurement_concept_id,
-	c.concept_name as type,
-	c.domain_id,
-	c.concept_class_id,
-	c.vocabulary_id
-from
-	measurement m
-	join person p on m.person_id = p.person_id
-	join concept c on m.measurement_concept_id = c.concept_id 
-	join concept tc on m.measurement_type_concept_id = tc.concept_id
-where 1=1
-	and tc.concept_id = 0
-order by 
-	c.domain_id,
-	c.concept_class_id,
-	c.vocabulary_id,
-	c.concept_name
-;
-
-
-
-select * from concept where concept_id = 3004249;
 
 select
-	measurement_type_concept_id,
+	v.visit_concept_id,
+	c.concept_name,
+	c.standard_concept,
+	c.domain_id,
+	count(*) as "count"
+from
+	visit_occurrence v
+	join concept c on c.concept_id = v.visit_concept_id 
+group by
+	v.visit_concept_id,
+	c.concept_name,
+	c.standard_concept,
+	c.domain_id
+order by count desc
+;
+
+
+select * from concept where concept_id = 32035;
+
+
+		SELECT 'VISIT_OCCURRENCE.ADMITTING_SOURCE_CONCEPT_ID' AS violating_field, cdmTable.* 
+		FROM synthea_micro.dbo.VISIT_OCCURRENCE cdmTable
+		
+		WHERE cdmTable.ADMITTING_SOURCE_CONCEPT_ID IS NULL;
+
+exec sp_columns visit_occurrence;
+
+select * from cdm_source;
+
+select * from concept where concept_id = 756265;
+
+select
+	v.admitted_from_concept_id,
 	count(*)
 from
-	measurement
-group by measurement_type_concept_id
-order by measurement_type_concept_id
+	visit_occurrence v
+group by v.admitted_from_concept_id
 ;
 
-select * from concept where concept_id in (4024958,3004249);
+update visit_occurrence set admitted_from_concept_id = 0 where admitted_from_concept_id is null;
 
--- measurment unit id
-
-
-select * from measurement where unit_concept_id = 0;
+select * from care_site;
 
 
 
 
-select distinct 
+	SELECT 'VISIT_OCCURRENCE.VISIT_CONCEPT_ID' AS violating_field, cdmTable.* 
+		  FROM synthea_micro.dbo.VISIT_OCCURRENCE cdmTable
+		  LEFT JOIN synthea_micro.dbo.concept co
+		    ON cdmTable.VISIT_CONCEPT_ID = co.concept_id
+		  
+		 WHERE co.concept_id != 0 AND co.domain_id NOT IN ('Visit')
+;
+
+select
+	v.visit_concept_id,
 	c.concept_name,
-	m.measurement_concept_id,
-	m.value_as_number,
-	c.concept_name as type,
+	c.standard_concept,
 	c.domain_id,
-	c.concept_class_id,
-	c.vocabulary_id
+	count(*) as "count"
 from
-	measurement m
-	join person p on m.person_id = p.person_id
-	join concept c on m.measurement_concept_id = c.concept_id 
-	join concept tc on m.measurement_type_concept_id = tc.concept_id
-where 1=1
-	and m.unit_concept_id = 0
-order by 
-	c.domain_id,
-	c.concept_class_id,
-	c.vocabulary_id,
-	c.concept_name
+	visit_occurrence v
+	join concept c on c.concept_id = v.visit_concept_id 
+group by
+	v.visit_concept_id,
+	c.concept_name,
+	c.standard_concept,
+	c.domain_id
+order by count desc
 ;
 
 
-
-
-select distinct 
-	unit_concept_id
-from 
-	measurement
-;
-
-select distinct 
-	m.unit_concept_id,
-	c.*
-from 
-	measurement m
-	join concept c on c.concept_id = m.unit_concept_id;
-;
-
-select * from concept 
-where 1=1
-	and vocabulary_id = 'UCUM'
---	and domain_id = 'Unit'
-	and lower(concept_name) like '%score%'
+select 
+	*
+from
+	concept
+where
+	lower(concept_name) like '%encounter for check up%'
 order by concept_name
 ;
 
 
-select * from concept where lower(concept_name) like '%score%'
+select * from concept where concept_id = 32271;
 
-select * from measurement where unit_concept_id = 0;
+select * from concept where concept_id = 32693;
 
+select * from provider;
 
-select distinct 
-	c.concept_name,
-	m.measurement_concept_id,
-	m.value_as_number,
-	m.unit_concept_id,
-	c.concept_name as type,
-	c.domain_id,
-	c.concept_class_id,
-	c.vocabulary_id
-from
-	measurement m
-	join person p on m.person_id = p.person_id
-	join concept c on m.measurement_concept_id = c.concept_id 
-	join concept tc on m.measurement_type_concept_id = tc.concept_id
-where 1=1
-	and m.unit_concept_id = 0
-order by 
-	c.domain_id,
-	c.concept_class_id,
-	c.vocabulary_id,
-	c.concept_name
-;
+truncate table provider;
+insert into provider (
+	provider_id,
+	provider_name
+) values (
+	1,
+	'Not Available'
+);
+
+select top 10 * from visit_occurrence;
 
 
-select distinct 
-	c.concept_name,
-	m.measurement_concept_id,
-	max(m.value_as_number),
-	m.unit_concept_id,
-	c.concept_name as type,
-	uc.concept_name as units,
-	c.domain_id,
-	c.concept_class_id,
-	c.vocabulary_id
-from
-	measurement m
-	join person p on m.person_id = p.person_id
-	join concept c on m.measurement_concept_id = c.concept_id 
-	join concept tc on m.measurement_type_concept_id = tc.concept_id
-	join concept uc on m.unit_concept_id = uc.concept_id
-where 1=1
-group by c.concept_name,2,4,5,6,7,8,9
-order by 
-	c.domain_id,
-	c.concept_class_id,
-	c.vocabulary_id,
-	c.concept_name
-;
 
--- concepts failing protein in plasma/blood [g/dL]
-select	
-	p.person_id,
-	p.person_source_value,
-	c.concept_name,
-	m.measurement_id,
-	m.measurement_concept_id,
-	m.value_as_number,
-	uc.concept_name as units
-from
-	measurement m
-	join person p on m.person_id = p.person_id
-	join concept c on m.measurement_concept_id = c.concept_id 
-	join concept uc on m.unit_concept_id = uc.concept_id
-where 1=1
---	and m.measurement_concept_id = 3001802
-	and lower(c.concept_name) like '%pressure%'
-order by 
-	p.person_source_value,
-	m.value_as_number desc
-;
-
+drop table if exists prev_visit_temp;
 select 
-	m.measurement_id,
-	m.measurement_concept_id,
-	m.unit_concept_id,
---	c.concept_name,
-	uc.*
+	person_id,
+	visit_start_date, 
+	visit_occurrence_id,
+	lag(visit_occurrence_id, 1) over (
+		partition by person_id
+		order by visit_start_date
+	) as preceding_visit_occurrence_id
+into prev_visit_temp
 from
-	measurement m
---	join concept c on m.measurement_concept_id = c.concept_id
-	join concept uc on m.unit_concept_id = uc.concept_id
-where 1=1
-	and m.measurement_id = 1901
+	visit_occurrence
 ;
 
+drop index if exists prev_visit_temp.prev_visit_temp_idx;
+create index prev_visit_temp_idx on prev_visit_temp(visit_occurrence_id);
 
-
-
-commit;
-
-select * from concept where concept_id = 0;
-
-select * from concept where lower(concept_name) like '%foo%'
-
-
-go
-begin transaction;
-
-commit transaction;
+update 
+	visit_occurrence
+set 
+	preceding_visit_occurrence_id = t.preceding_visit_occurrence_id
+from 
+	visit_occurrence v
+	join prev_visit_temp t on t.visit_occurrence_id = v.visit_occurrence_id
+;
 
 select count(*) from person;
 
-select * from concept where concept_id > 2000000000;
+update visit_occurrence set preceding_visit_occurrence_id = visit_occurrence_id where preceding_visit_occurrence_id is null;
 
-
-
-update concept set concept_name = concept_name where concept_id in (select max(concept_id) from concept);
-
-select max(person_id) from person;
-
-
-create sequence concept_concept_id
-start with 2000000000
-increment by 1
-;
-
-select next value for concept_concept_id as foo;
+		SELECT 'DRUG_EXPOSURE.DRUG_CONCEPT_ID' AS violating_field, cdmTable.* 
+		  FROM synthea_micro.dbo.DRUG_EXPOSURE cdmTable
+		  
+		  join synthea_micro.dbo.concept co ON cdmTable.DRUG_CONCEPT_ID = co.concept_id
+		  WHERE co.concept_id != 0 AND (co.standard_concept != 'S' OR co.invalid_reason IS NOT NULL ) 
 
 
 select distinct
---	m.measurement_concept_id,
-	c.concept_name as measurement_name,
---	m.unit_concept_id,
---	c.concept_name,
-	uc.concept_name as units_concept_name,
-	uc.*
+	c.*
 from
-	measurement m
-	join concept c on m.measurement_concept_id = c.concept_id
-	join person p on m.person_id = p.person_id
-	join concept uc on m.unit_concept_id = uc.concept_id
-where 1=1
-	and uc.concept_id > 2000000000
-order by concept_name
+	drug_exposure d
+	join concept c on d.drug_concept_id = c.concept_id 
+where
+	c.standard_concept != 'S'
+;
+
+select
+	standard_concept,
+	count(*) cnt
+from
+	concept 
+group by standard_concept
+order by standard_concept
 ;
 
 
-delete from concept where concept_id > 2000000000;
+select * from concept where concept_id = 38000177;
 
-select * from concept where concept_id >= 2000000000;
+select count(*) from condition_occurrence;
+
+select top 100 * from VISIT_OCCURRENCE;
 
 select count(*) from person;
 
-select concept_id, count(*) cnt from concept
-where concept_id > 2000000000
-group by concept_id
-order by cnt desc;
 
-select vocabulary_id, concept_code, count(*) cnt from concept where concept_id > 2000000000
-group by vocabulary_id, concept_code
-order by cnt desc;
 
-select distinct 
-	p.person_source_value,
-	mc.concept_name as measurement,
-	uc.concept_name as units
+		SELECT 'OBSERVATION.UNIT_CONCEPT_ID' AS violating_field, cdmTable.* 
+		FROM synthea_micro.dbo.OBSERVATION cdmTable
+		
+		WHERE cdmTable.UNIT_CONCEPT_ID IS NULL
+
+select distinct
+	o.observation_concept_id,
+	c.*
 from
-	measurement m
-	join person p on m.person_id = p.person_id 
-	join concept mc on mc.concept_id = m.measurement_concept_id
-	join concept uc on uc.concept_id = m.unit_concept_id
+	observation o
+	join concept c on o.observation_concept_id = c.concept_id 
 where
-	uc.concept_id > 2000000000
-order by 
-	p.person_source_value
-;
+	unit_concept_id is null
 
 
-
-		SELECT 'MEASUREMENT.UNIT_CONCEPT_ID' AS violating_field, cdmTable.* 
-		  FROM synthea_micro.dbo.MEASUREMENT cdmTable
+		SELECT 'OBSERVATION.UNIT_CONCEPT_ID' AS violating_field, cdmTable.* 
+		  FROM synthea_micro.dbo.OBSERVATION cdmTable
+		  LEFT JOIN synthea_micro.dbo.concept co
+		    ON cdmTable.UNIT_CONCEPT_ID = co.concept_id
 		  
-		  join synthea_micro.dbo.concept co ON cdmTable.UNIT_CONCEPT_ID = co.concept_id
+		 WHERE co.concept_id != 0 AND co.domain_id NOT IN ('Unit')
+
+
+
+		SELECT 'OBSERVATION.OBSERVATION_CONCEPT_ID' AS violating_field, cdmTable.* 
+		  FROM synthea_micro.dbo.OBSERVATION cdmTable
+		  
+		  join synthea_micro.dbo.concept co ON cdmTable.OBSERVATION_CONCEPT_ID = co.concept_id
 		  WHERE co.concept_id != 0 AND (co.standard_concept != 'S' OR co.invalid_reason IS NOT NULL ) 
 
-select * from concept where concept_id = 8773;
 
 
-		SELECT 'MEASUREMENT.MEASUREMENT_CONCEPT_ID' AS violating_field, cdmTable.* 
-		FROM synthea_micro.dbo.MEASUREMENT cdmTable
-		
-		LEFT JOIN synthea_micro.dbo.CONCEPT fkTable
-		ON cdmTable.MEASUREMENT_CONCEPT_ID = fkTable.CONCEPT_ID
-		WHERE fkTable.CONCEPT_ID IS NULL AND cdmTable.MEASUREMENT_CONCEPT_ID IS NOT NULL 
 
-		SELECT 'MEASUREMENT.UNIT_CONCEPT_ID' AS violating_field, cdmTable.* 
-		FROM synthea_micro.dbo.MEASUREMENT cdmTable
-		
-		LEFT JOIN synthea_micro.dbo.CONCEPT fkTable
-		ON cdmTable.UNIT_CONCEPT_ID = fkTable.CONCEPT_ID
-		WHERE fkTable.CONCEPT_ID IS NULL AND cdmTable.UNIT_CONCEPT_ID IS NOT NULL 
+select * from concept where concept_id = 40623280;
+
+select distinct o.observation_concept_id, c.*
+		FROM synthea_micro.dbo.OBSERVATION o
+		join concept c on c.concept_id = o.observation_concept_id
+		WHERE o.OBSERVATION_SOURCE_VALUE IS NULL
+
+
+
+
+
+select * from concept where lower(concept_name) like 'procedure_occurrence' 
+and domain_id = 'Metadata'
+union all
+select * from concept where lower(concept_name) like 'observation' 
+and domain_id = 'Metadata'
+order by domain_id;
+
+
+select distinct domain_id from concept
+order by domain_id;
+
+select * from location;
+
+truncate table location;
+
+insert into location (
+	location_id,
+	address_1,
+	city,
+	state,
+	zip,
+	county,
+	location_source_value,
+	country_concept_id,
+	country_source_value
+) values (
+	1,
+	'Location Unknow',
+	'Unknown City',
+	'??',
+	'00000',
+	'Unknown County',
+	'Unknown',
+	42046186,
+	'USA'
+)
+
+select * from concept where lower(concept_name) like 'united states'
+
+select distinct concept_class_id from concept order by concept_class_id;
+
+
+select * from provider;
+
+
+SELECT 'PERSON.RACE_CONCEPT_ID' AS violating_field, cdmTable.* 
+		  FROM synthea_micro.dbo.PERSON cdmTable
+		  
+		  join synthea_micro.dbo.concept co ON cdmTable.RACE_CONCEPT_ID = co.concept_id
+		  WHERE co.concept_id != 0 AND (co.standard_concept != 'S' OR co.invalid_reason IS NOT NULL ) 
+
+
+
+select * from concept where concept_id = 8522;
+
+select * from visit_detail;
+
 
 
