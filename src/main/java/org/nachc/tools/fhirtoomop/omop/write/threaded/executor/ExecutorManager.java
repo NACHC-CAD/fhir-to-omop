ee18a3490af0dbdd56de3f511f26bb3428a0c390
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-04-23 11:53:28 -0400

    SWITCHED FROM THREAD TO EXECUTOR (fixes for integration tests)

--- src/main/java/org/nachc/tools/fhirtoomop/omop/write/threaded/executor/ExecutorManager.java
+++ src/main/java/org/nachc/tools/fhirtoomop/omop/write/threaded/executor/ExecutorManager.java
@@ -7,17 +7,22 @@ import org.nachc.tools.fhirtoomop.util.params.AppParams;
 
 public class ExecutorManager {
 
-	private static ThreadPoolExecutor writerExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(AppParams.getMaxNumberOfThreadsForUpload());
+	private static ThreadPoolExecutor writerExecutor;
 	
-	private static ThreadPoolExecutor workerExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(AppParams.getMaxNumberOfThreadsForUpload());
+	private static ThreadPoolExecutor workerExecutor;
 
 	public static ThreadPoolExecutor getWriterExecutor() {
+		if(writerExecutor == null || writerExecutor.isShutdown() == true) {
+			writerExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(AppParams.getMaxNumberOfThreadsForUpload());
+		}
 		return writerExecutor;
 	}
 
 	public static ThreadPoolExecutor getWorkerExecutor() {
+		if(workerExecutor == null || workerExecutor.isShutdown() == true) {
+			workerExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(AppParams.getMaxNumberOfThreadsForUpload());
+		}
 		return workerExecutor;
 	}
 
-	
 }
