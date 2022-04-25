21f53ae4b37f4d75cd12d246877616bc83ec44a7
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-04-25 19:49:22 -0400

    more on documentation

--- src/main/java/org/nachc/tools/fhirtoomop/tools/download/DownloadPatients.java
+++ src/main/java/org/nachc/tools/fhirtoomop/tools/download/DownloadPatients.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.nachc.tools.fhirtoomop.tools.download.patient.download.FhirPatientDownloader;
 import org.nachc.tools.fhirtoomop.util.params.AppParams;
+import org.yaorma.util.time.Timer;
 
 import com.nach.core.util.file.FileUtil;
 
@@ -15,16 +16,24 @@ import lombok.extern.slf4j.Slf4j;
 public class DownloadPatients {
 
 	private static String DIR_PATH = AppParams.getDownloadPatientIdDir();
-	
+
 	public static void main(String[] args) {
 		getPatients();
 	}
-	
+
 	private static void getPatients() {
 		log.info("Getting patients...");
+		Timer timer = new Timer();
+		timer.start();
 		List<String> patientList = getPatientList();
 		log.info("Got " + patientList.size() + " patients.");
 		FhirPatientDownloader.getPatients(patientList);
+		log.info("");
+		log.info("-----------------------");
+		log.info("start:   " + timer.getStartAsString());
+		log.info("end:     " + timer.getStopAsString());
+		log.info("elapsed: " + timer.getElapsedString());
+		log.info("-----------------------");
 		log.info("Done.");
 	}
 
