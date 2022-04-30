b75fe04b43b07a44e2daf7baded51000fb2a7ce2
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-04-30 12:17:25 -0400

    Adding standard concept cache (work in progress)

---
+++ src/main/java/org/nachc/tools/fhirtoomop/util/mapping/impl/cache/StandardConceptCache.java
@@ -0,0 +1,54 @@
+package org.nachc.tools.fhirtoomop.util.mapping.impl.cache;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.HashMap;
+
+import org.nachc.tools.omop.yaorma.dvo.ConceptDvo;
+import org.yaorma.dao.Dao;
+import org.yaorma.database.Database;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StandardConceptCache {
+
+	private static HashMap<CacheKey, ConceptDvo> concepts;
+
+	public static void init(Connection conn) {
+		ResultSet rs = null;
+		try {
+			log.info("CREATING STANDARD_CONCEPT_CACHE...");
+			if (concepts == null) {
+				concepts =  new HashMap<CacheKey, ConceptDvo>();
+				log.info("Querying database...");
+				String sqlString = "select * from concept where standard_concept = 'S'";
+				rs = Database.executeQuery(sqlString, conn);
+				log.info("Creating cache...");
+				int cnt = 0;
+				while (rs.next()) {
+					cnt++;
+					ConceptDvo dvo = new ConceptDvo();
+					Dao.load(dvo, rs);
+					CacheKey key = new CacheKey(dvo.getVocabularyId(), dvo.getConceptCode());
+					concepts.put(key, dvo);
+					if(cnt % 100000 == 0) {
+						log.info("Got " + cnt + " concepts");
+					}
+				}
+				log.info("Got result set...");
+			} else {
+				log.info("Concept cache has already been initialted: (" + concepts.size() + ") standard concepts.");
+			}
+		} catch(Exception exp) {
+			throw new RuntimeException(exp);
+		} finally {
+			try {
+				rs.close();
+			} catch (Exception exp) {
+				throw new RuntimeException(exp);
+			}
+		}
+	}
+
+}
---
+++ src/test/java/org/nachc/tools/fhirtoomop/util/mapping/impl/cache/StandardConceptCacheIntegrationTest.java
@@ -0,0 +1,32 @@
+package org.nachc.tools.fhirtoomop.util.mapping.impl.cache;
+
+import java.sql.Connection;
+
+import org.junit.Test;
+import org.nachc.tools.fhirtoomop.util.db.connection.OmopDatabaseConnectionFactory;
+import org.yaorma.util.time.Timer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StandardConceptCacheIntegrationTest {
+
+	@Test
+	public void shouldCreateCache() {
+		log.info("Starting test...");
+		Connection conn = OmopDatabaseConnectionFactory.getOmopConnection();
+		Timer timer = new Timer();
+		try {
+			timer.start();
+			StandardConceptCache.init(conn);
+			timer.stop();
+		} finally {
+			OmopDatabaseConnectionFactory.close(conn);
+		}
+		log.info("--------------------");
+		log.info("elapsed: " + timer.getElapsedString());
+		log.info("--------------------");
+		log.info("Done.");
+	}
+	
+}
