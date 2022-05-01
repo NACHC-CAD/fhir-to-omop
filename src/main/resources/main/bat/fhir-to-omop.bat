49f9eecfde6204b692ab5af49707a87c0b113a99
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-04-30 20:40:57 -0400

    update to bat file

--- src/main/resources/main/bat/fhir-to-omop.bat
+++ src/main/resources/main/bat/fhir-to-omop.bat
@@ -12,7 +12,7 @@ set workingDir=%CD%
 
 echo Working Directory: %workingDir%
 
-java -jar -Xms1g -Xmx8g fhir-to-omop.jar %1 %2
+java -jar  -Dosgi.requiredJavaVersion=1.8 -Dosgi.instance.area.default=@user.home/eclipse-workspace -XX:+UseG1GC -XX:+UseStringDeduplication -Dosgi.requiredJavaVersion=11 -Dosgi.dataAreaRequiresExplicitInit=true -Xms1g -Xmx16g fhir-to-omop.jar %1 %2
 
 echo.
 echo.
