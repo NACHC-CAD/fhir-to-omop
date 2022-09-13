243df69f9e8ff2f494bfae060e5a82218c38dae3
greshje <greshje@gmail.com>
2022-09-13 14:23:00 -0400

    work in progress

---
+++ src/main/java/org/nachc/tools/fhirtoomop/fhir/parser/r4/valueset/ValueSetParser.java
@@ -0,0 +1,22 @@
+package org.nachc.tools.fhirtoomop.fhir.parser.r4.valueset;
+
+import org.hl7.fhir.dstu3.model.ValueSet;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ValueSetParser {
+	
+	private ValueSet valueSet;
+	
+	public ValueSetParser(ValueSet valueSet) {
+		this.valueSet = valueSet;
+	}
+	
+	public String getName() {
+		return valueSet.getName();
+	}
+
+}
