5708f27485248505997fde6f72e169201ba1870d
greshje <greshje@gmail.com>
2022-09-14 11:48:46 -0400

    work in progress

--- src/main/java/org/nachc/tools/fhirtoomop/fhir/parser/r4/valueset/ValueSetParser.java
+++ src/main/java/org/nachc/tools/fhirtoomop/fhir/parser/r4/valueset/ValueSetParser.java
@@ -1,10 +1,19 @@
 package org.nachc.tools.fhirtoomop.fhir.parser.r4.valueset;
 
-import org.hl7.fhir.dstu3.model.ValueSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.ValueSet;
+import org.hl7.fhir.r4.model.ValueSet.ConceptReferenceComponent;
+import org.hl7.fhir.r4.model.ValueSet.ConceptSetComponent;
+import org.hl7.fhir.r4.model.ValueSet.ValueSetExpansionContainsComponent;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Getter
 @Setter
 public class ValueSetParser {
@@ -19,4 +28,42 @@ public class ValueSetParser {
 		return valueSet.getName();
 	}
 
+	public List<Coding> getConcepts() {
+		ArrayList<Coding> rtn = new ArrayList<>();
+		List<ConceptSetComponent> component = valueSet.getCompose().getInclude();
+		log.info("Got " + component.size() + " values");
+		log.info("Got values");
+		for(ConceptSetComponent comp : component) {
+			List<ConceptReferenceComponent> concepts =  comp.getConcept();
+			for(ConceptReferenceComponent concept : concepts) {
+				Coding coding = new Coding();
+				coding.setSystem(comp.getSystem());
+				coding.setCode(concept.getCode());
+				coding.setDisplay(concept.getDisplay());
+				rtn.add(coding);
+			}
+		}
+		List<ValueSetExpansionContainsComponent> expansionList = valueSet.getExpansion().getContains();
+		for(ValueSetExpansionContainsComponent expansion : expansionList) {
+			Coding coding = new Coding();
+			coding.setSystem(expansion.getSystem());
+			coding.setCode(expansion.getCode());
+			coding.setDisplay(expansion.getDisplay());
+			rtn.add(coding);
+		}
+		return rtn;
+	}
+	
+	public String getCodesAsCsv() {
+		String rtn = "";
+		List<Coding> codingList = this.getConcepts();
+		for(Coding coding : codingList) {
+			if("".equals(rtn) == false) {
+				rtn += ",";
+			}
+			rtn += coding.getCode();
+		}
+		return rtn;
+	}
+	
 }
