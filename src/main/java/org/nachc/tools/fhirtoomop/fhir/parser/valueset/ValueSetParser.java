ab0c54796a4d6402b0ed4efd0e48c0c074a7b273
greshje <greshje@gmail.com>
2022-11-09 22:18:14 -0500

    added functionality to ValueSetParser (Dstu3)

--- src/main/java/org/nachc/tools/fhirtoomop/fhir/parser/valueset/ValueSetParser.java
+++ src/main/java/org/nachc/tools/fhirtoomop/fhir/parser/valueset/ValueSetParser.java
@@ -1,12 +1,21 @@
 package org.nachc.tools.fhirtoomop.fhir.parser.valueset;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hl7.fhir.dstu3.model.ValueSet;
+import org.hl7.fhir.dstu3.model.Coding;
+import org.hl7.fhir.dstu3.model.ValueSet.ConceptReferenceComponent;
+import org.hl7.fhir.dstu3.model.ValueSet.ConceptSetComponent;
+import org.hl7.fhir.dstu3.model.ValueSet.ValueSetExpansionContainsComponent;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Setter
+@Slf4j
 public class ValueSetParser {
 	
 	private ValueSet valueSet;
@@ -19,4 +28,30 @@ public class ValueSetParser {
 		return valueSet.getName();
 	}
 
+	public List<Coding> getConcepts() {
+		ArrayList<Coding> rtn = new ArrayList<>();
+		List<ConceptSetComponent> component = valueSet.getCompose().getInclude();
+		log.info("Got " + component.size() + " values");
+		log.info("Got values");
+		for(ConceptSetComponent comp : component) {
+			List<ConceptReferenceComponent> concepts =  comp.getConcept();
+			for(ConceptReferenceComponent concept : concepts) {
+				Coding coding = new Coding();
+				coding.setSystem(comp.getSystem());
+				coding.setCode(concept.getCode());
+				coding.setDisplay(concept.getDisplay());
+				rtn.add(coding);
+			}
+		}
+		List<ValueSetExpansionContainsComponent> expansionList = valueSet.getExpansion().getContains();
+		for(ValueSetExpansionContainsComponent expansion : expansionList) {
+			Coding coding = new Coding();
+			coding.setSystem(expansion.getSystem());
+			coding.setCode(expansion.getCode());
+			coding.setDisplay(expansion.getDisplay());
+			rtn.add(coding);
+		}
+		return rtn;
+	}
+	
 }
