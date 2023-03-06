954dfa3fbf29909c5878b7a6397ee730b27fa69b
NACHC-CAD <greshje.nachc@gmail.com>
2023-03-06 03:43:33 -0500

    added create tables for synthea native

--- src/main/java/org/nachc/tools/fhirtoomop/tools/build/postgres/build/VOC99_LoadTerminology.java
+++ src/main/java/org/nachc/tools/fhirtoomop/tools/build/postgres/build/VOC99_LoadTerminology.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import org.nachc.tools.fhirtoomop.util.db.connection.postgres.PostgresDatabaseConnectionFactory;
 import org.nachc.tools.fhirtoomop.util.params.AppParams;
 import org.yaorma.database.Database;
+import org.yaorma.util.time.Timer;
 
 import com.nach.core.util.file.FileUtil;
 
@@ -23,6 +24,8 @@ public class VOC99_LoadTerminology {
 
 	public static void exec() {
 		log.info("Loading terminologies...");
+		Timer timer = new Timer();
+		timer.start();
 		Connection conn = PostgresDatabaseConnectionFactory.getOhdsiConnection();
 		log.info("Got connection...");
 		try {
@@ -35,6 +38,8 @@ public class VOC99_LoadTerminology {
 		} finally {
 			Database.close(conn);
 		}
+		timer.stop();
+		log.info("TIME TO LOAD TERMINOLOGIES: " + timer.getElapsedString());
 		log.info("Done loading terminologies...");
 	}
 
