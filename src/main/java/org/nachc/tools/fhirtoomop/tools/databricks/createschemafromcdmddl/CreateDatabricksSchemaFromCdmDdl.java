8e0fc39a26c7b3542b6b751faaafbc01438fbb6a
NACHC-CAD <greshje.nachc@gmail.com>
2023-04-23 14:12:55 -0400

    updates for databricks

--- src/main/java/org/nachc/tools/fhirtoomop/tools/databricks/createschemafromcdmddl/CreateDatabricksSchemaFromCdmDdl.java
+++ src/main/java/org/nachc/tools/fhirtoomop/tools/databricks/createschemafromcdmddl/CreateDatabricksSchemaFromCdmDdl.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.sql.Connection;
 
 import org.nachc.tools.fhirtoomop.util.databricks.connection.DatabricksConnectionFactory;
+import org.nachc.tools.fhirtoomop.util.databricks.database.DatabricksDatabase;
 import org.nachc.tools.fhirtoomop.util.databricks.properties.DatabricksProperties;
 import org.yaorma.database.Database;
 
@@ -38,10 +39,10 @@ public class CreateDatabricksSchemaFromCdmDdl {
 		sqlString = replace(sqlString, "@cdmDatabaseSchema", schemaName);
 		// drop the existing schema
 		log.info("Dropping existing database...");
-		Database.update("drop database if exists " + schemaName + " cascade", conn);
+		DatabricksDatabase.update("drop database if exists " + schemaName + " cascade", conn);
 		// create the database
 		log.info("Creating the database...");
-		Database.update("create database " + schemaName, conn);
+		DatabricksDatabase.update("create database " + schemaName, conn);
 		// populate the database from the ddl sql
 		log.info("Creating database objects...");
 		Database.executeSqlScript(sqlString, conn);
