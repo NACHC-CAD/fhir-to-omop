16985519487309c2a664f8a2dfd7192aeaf7d047
NACHC-CAD <greshje.nachc@gmail.com>
2023-06-05 21:35:57 -0400

    work in progress on databricks stuff

--- src/main/java/org/nachc/tools/fhirtoomop/tools/databricks/OhdsiEnableExistingDatabricksCdm.java
+++ src/main/java/org/nachc/tools/fhirtoomop/tools/databricks/OhdsiEnableExistingDatabricksCdm.java
@@ -12,7 +12,7 @@ import org.nachc.tools.fhirtoomop.tools.databricks.build.DBR06_UploadAchillesAna
 import org.nachc.tools.fhirtoomop.tools.databricks.build.DBR07_RunAchillesScript;
 import org.nachc.tools.fhirtoomop.tools.databricks.build.DBR08_DeleteAchillesWebApiRecords;
 import org.nachc.tools.fhirtoomop.tools.databricks.build.DBR09_CreateAchillesWebApiRecords;
-import org.nachc.tools.fhirtoomop.util.databricks.connection.DatabricksConnectionFactory;
+import org.nachc.tools.fhirtoomop.tools.databricks.connection.webapi.DatabricksWebApiConnectionFactory;
 import org.nachc.tools.fhirtoomop.util.databricks.database.DatabricksDatabase;
 import org.nachc.tools.fhirtoomop.util.databricks.properties.DatabricksProperties;
 import org.yaorma.util.time.Timer;
@@ -23,17 +23,25 @@ import lombok.extern.slf4j.Slf4j;
 public class OhdsiEnableExistingDatabricksCdm {
 
 	public static void main(String[] args) {
+		exec();
+		log.info("Done.");
+	}
+
+	public static void exec() {
+		// delete and create the database to hold the webapi schema (required bootstrap connection)
+		DBR00a_CreateOhdsiDatabaseForWebApiSchema.exec();
+		// build the webapi schema
 		Connection conn = null;
 		try {
 			log.info("Getting connection...");
-			conn = DatabricksConnectionFactory.getConnection();
+			conn = DatabricksWebApiConnectionFactory.getConnection();
 			log.info("Building Databricks instance...");
 			String databricksFilesRoot = DatabricksProperties.getDatabricksFilesRoot();
 			String schemaName = DatabricksProperties.getSchemaName();
 			String vocabSchemaName = DatabricksProperties.getVocabSchemaName();
 			String achillesTempSchemaName = DatabricksProperties.getAchillesTempSchemaName();
 			String achillesResultsSchemaName = DatabricksProperties.getAchillesResultsSchemaName();
-			exec(databricksFilesRoot, schemaName, vocabSchemaName, achillesTempSchemaName, achillesResultsSchemaName, conn);
+			buildWebApi(databricksFilesRoot, schemaName, vocabSchemaName, achillesTempSchemaName, achillesResultsSchemaName, conn);
 			log.info("Done building instance.");
 		} finally {
 			try {
@@ -42,15 +50,13 @@ public class OhdsiEnableExistingDatabricksCdm {
 				log.info("An exception was thrown trying to close the connection. This seems to happen with Databricks sometimes");
 			}
 		}
-		log.info("Done.");
 	}
 
-	public static void exec(String databricksFilesRoot, String schemaName, String vocabSchemaName, String achillesTempSchemaName, String achillesResultsSchemaName, Connection conn) {
+	private static void buildWebApi(String databricksFilesRoot, String schemaName, String vocabSchemaName, String achillesTempSchemaName, String achillesResultsSchemaName, Connection conn) {
 		// timer
 		Timer timer = new Timer();
 		timer.start();
 		// create the webapi schema
-		DBR00a_CreateOhdsiDatabaseForWebApiSchema.exec();
 		DBR00b_CreateAtlasDatabaseUsers.exec(conn);
 		DBR00c_CreateAtlasWebApiSchema.exec(conn);
 		DBR00d_CreateAtlasWebApiTables.exec(conn);
