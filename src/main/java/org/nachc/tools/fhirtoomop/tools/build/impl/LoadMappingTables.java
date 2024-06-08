52ccbf669cb5ce67049c64a6b9c85f8a0df86cb5
greshje <greshje@gmail.com>
2024-06-08 19:17:10 -0400

    work in progress on adding Polites functionality for SqlServer

--- src/main/java/org/nachc/tools/fhirtoomop/tools/build/impl/LoadMappingTables.java
+++ src/main/java/org/nachc/tools/fhirtoomop/tools/build/impl/LoadMappingTables.java
@@ -15,15 +15,22 @@ import lombok.extern.slf4j.Slf4j;
 public class LoadMappingTables {
 
 	public static void exec(File sqlFile, Connection conn) {
+		File dir = new File(sqlFile.getParent());
+		File raceFile = new File(dir, "RaceAndEthnicityCDC-OMOP-MAPPING-Race.txt");
+		File ethFile = new File(dir, "RaceAndEthnicityCDC-OMOP-MAPPING-Eth.txt");
 		log.info("-----------------------");
 		log.info("LOAD RACE/ETH SQL FILE: " + FileUtil.getCanonicalPath(sqlFile));
 		log.info("-----------------------");
 		InputStream IS = FileUtil.getInputStream(sqlFile);
+		String sqlString = FileUtil.getAsString(sqlFile);
+		sqlString = sqlString.replace("<RACE_FILE>", FileUtil.getCanonicalPath(raceFile));
+		sqlString = sqlString.replace("<ETH_FILE>", FileUtil.getCanonicalPath(ethFile));
+		log.info("SQL STRING: \n" + sqlString);
 		String dbName = AppParams.getSchemaName();
 		log.info("Using: " + dbName);
 		Database.update("use " + dbName, conn);
 		log.info("Running script...");
-		Database.executeSqlScript(IS, conn);
+		Database.executeSqlScript(sqlString, conn);
 		log.info("Done running script.");
 		log.info("Done creating database tables.");
 	}
