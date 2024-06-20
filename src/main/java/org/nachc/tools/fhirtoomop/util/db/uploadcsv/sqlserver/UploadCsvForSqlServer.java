1b54b1e1c32c549e2e49e04d40c54191587583d7
greshje <greshje@gmail.com>
2024-06-19 20:30:21 -0400

    about to refactor upload cdm csv

---
+++ src/main/java/org/nachc/tools/fhirtoomop/util/db/uploadcsv/sqlserver/UploadCsvForSqlServer.java
@@ -0,0 +1,17 @@
+package org.nachc.tools.fhirtoomop.util.db.uploadcsv.sqlserver;
+
+public class UploadCsvForSqlServer {
+
+	public static void uploadAll() {
+		// TODO
+	}
+	
+	public static void uploadDatatables() {
+		// TODO
+	}
+	
+	public static void uploadTerminologyTables() {
+		// TODO
+	}
+	
+}
