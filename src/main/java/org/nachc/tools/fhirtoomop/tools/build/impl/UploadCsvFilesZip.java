1dec75e335db445a171c4c4e0bccfcdc78270969
greshje <greshje@gmail.com>
2024-06-19 21:29:07 -0400

    upload all for cdm csv working

--- src/main/java/org/nachc/tools/fhirtoomop/tools/build/impl/UploadCsvFilesZip.java
+++ src/main/java/org/nachc/tools/fhirtoomop/tools/build/impl/UploadCsvFilesZip.java
@@ -3,6 +3,7 @@ package org.nachc.tools.fhirtoomop.tools.build.impl;
 import java.io.File;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.util.ArrayList;
 
 import org.yaorma.database.Database;
 
@@ -14,64 +15,133 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class UploadCsvFilesZip {
 
-	public static void exec(Connection conn, InputStream is, File outputDir) {
+	private ArrayList<String> ignoreList = new ArrayList<String>();
+	
+	private ArrayList<String> success = new ArrayList<String>();
+	
+	private ArrayList<String> failure = new ArrayList<String>();
+	
+	private ArrayList<String> ignored = new ArrayList<String>();
+	
+	private boolean invertIgnore = false;
+
+	public void exec(String dbName, String schemaName, InputStream is, File outputDir, Connection conn) {
 		try {
-			log.info("Cleaning up files...");
-			FileUtil.rmdir(outputDir);
-			FileUtil.mkdirs(outputDir);
 			log.info("Unzipping file...");
 			ZipUtil.unzip(is, outputDir);
 			log.info("Unzipped to: " + FileUtil.getCanonicalPath(outputDir));
+			// get the list of files
 			File srcDir = outputDir;
 			File[] files = srcDir.listFiles();
 			log.info("Got " + files.length + " files...");
 			for(File file : files) {
-				if(file.isDirectory() == false) {
-					uploadFile(conn, file);
+				if(file.isDirectory() == false && isZip(file) == false) {
+					uploadFile(dbName, schemaName, file, conn);
 				}
 			}
-			log.info("-----------");
+			logOutcomes();
 			log.info("Done uploading files.");
 		} catch(Exception exp) {
 			throw new RuntimeException(exp);
-		} finally {
-			FileUtil.rmdir(outputDir);
 		}
 	}
 
-	private static void uploadFile(Connection conn, File file) {
+	private void uploadFile(String dbName, String schemaName, File file, Connection conn) {
 		String fileName = file.getName();
 		String tableName = fileName.substring(0, fileName.indexOf("."));
+		String fullName = dbName + "." + schemaName + "." + tableName;
 		log.info("-----------");
 		log.info("Uploading file: " + FileUtil.getCanonicalPath(file));
-		log.info("Table Name: " + tableName);
-		if(skip(tableName) == true) {
-			log.info("SKIPPING TABLE: " + tableName);
+		log.info("Table Name: " + fullName);
+		if(ignore(tableName) == true) {
+			log.info("SKIPPING TABLE: " + fullName);
+			this.ignored.add(fullName);
 			return;
 		}
-		String sqlString = "";
-		sqlString += "BULK INSERT " + tableName + " \n";
-		sqlString += "FROM '" + FileUtil.getCanonicalPath(file) + "' \n";
-		sqlString += "WITH \n";
-		sqlString += "( \n";
-		sqlString += "    FIELDTERMINATOR = ',', \n";
-		sqlString += "    ROWTERMINATOR = '\n', \n";
-		sqlString += "    FIRSTROW = 2, \n";
-		sqlString += "    FORMAT = 'CSV', \n";
-		sqlString += "    TABLOCK \n";
-		sqlString += ") \n";		Database.update("truncate table " + tableName, conn);
-		Database.update(sqlString, conn);
+		try {
+			String sqlString = "";
+			sqlString += "BULK INSERT " + fullName + " \n";
+			sqlString += "FROM '" + FileUtil.getCanonicalPath(file) + "' \n";
+			sqlString += "WITH \n";
+			sqlString += "( \n";
+			sqlString += "    FIELDTERMINATOR = ',', \n";
+			sqlString += "    ROWTERMINATOR = '\n', \n";
+			sqlString += "    FIRSTROW = 2, \n";
+			sqlString += "    FORMAT = 'CSV', \n";
+			sqlString += "    TABLOCK \n";
+			sqlString += ") \n";		
+			Database.update("use " + dbName, conn);
+			Database.update(sqlString, conn);
+			this.success.add(fullName);
+		} catch(Exception exp) {
+			log.info("COULD NOT UPLOAD FILE (EXCEPTION OCCURRED): ");
+			log.info(exp.getMessage());
+			this.failure.add(fullName);
+		}
 		log.info("Done with file: " + FileUtil.getCanonicalPath(file));
 	}
 
-	private static boolean skip(String tableName) {
-		if("concept_recommended".equals(tableName)) {
-			return true;
-		} else if("attribute_definition".equals(tableName)) {
+	private boolean ignore(String tableName) {
+		boolean rtn = false;
+		if(this.ignoreList.contains(tableName.toUpperCase())) {
+			rtn = true;
+		} else {
+			rtn = false;
+		}
+		if(invertIgnore == true) {
+			rtn = !rtn;
+		}
+		return rtn;
+	}
+	
+	private boolean isZip(File file) {
+		String name = file.getName().toLowerCase();
+		if(name.endsWith(".zip") == true) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
+	//
+	// log the outcome
+	//
+	
+	public void logOutcomes() {
+		String msg;
+		msg = "\n--------------------\n";
+		msg += "Success: The following tables were successfully uploaded. \n";
+		for (String str : success) {
+			msg += "  " + str + "\n";
+		}
+		msg += "--------------------\n";
+		log.info(msg);
+		if (ignored.size() > 0) {
+			msg = "\n--------------------\n";
+			msg += "The following files were ignored: \n";
+			for (String str : ignored) {
+				msg += "  " + str + "\n";
+			}
+			msg += "--------------------\n";
+		} else {
+			msg = "\n--------------------\n";
+			msg += "No files were ignored.\n";
+			msg += "--------------------\n";
+		}
+		log.info(msg);
+		if (failure.size() > 0) {
+			msg = "\n--------------------\n";
+			msg += "! ! ! SOME TABLES FAILED ! ! !\n";
+			for (String str : failure) {
+				msg += "  " + str + "\n";
+			}
+			msg += "--------------------\n";
+		} else {
+			msg = "\n--------------------\n";
+			msg += "There were no errors.\n";
+			msg += "--------------------\n";
+		}
+		log.info(msg);
+	}
+
 }
--- src/main/java/org/nachc/tools/fhirtoomop/util/db/uploadcsv/sqlserver/UploadCsvForSqlServer.java
+++ src/main/java/org/nachc/tools/fhirtoomop/util/db/uploadcsv/sqlserver/UploadCsvForSqlServer.java
@@ -1,9 +1,55 @@
 package org.nachc.tools.fhirtoomop.util.db.uploadcsv.sqlserver;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+
+import org.nachc.tools.fhirtoomop.tools.build.impl.UploadCsvFilesZip;
+import org.nachc.tools.fhirtoomop.util.db.connection.BootstrapConnectionFactory;
+import org.nachc.tools.fhirtoomop.util.params.AppParams;
+import org.yaorma.database.Database;
+
+import com.nach.core.util.file.FileUtil;
+import com.nach.core.util.http.HttpRequestClient;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class UploadCsvForSqlServer {
 
+	private static final String ZIP_DIR = AppParams.getCdmCsvZipFileLocation();
+	
+	private static final String ZIP_NAME = AppParams.getCdmCsvZipFileName();
+	
+	private static final String URL = AppParams.getCdmCsvDownloadUrl();
+	
+	private static final boolean DOWNLOAD = AppParams.getCdmCsvDownloadIfNotFound();
+
+	private static final String DB = AppParams.getDatabaseName();
+	
+	private static final String SCHEMA = AppParams.getSchemaName();
+	
+	public static void main(String[] args) {
+		uploadAll();
+	}
+	
 	public static void uploadAll() {
-		// TODO
+		Connection conn = BootstrapConnectionFactory.getBootstrapConnection();
+		File file = null;
+		try {
+			download();
+			UploadCsvFilesZip upload = new UploadCsvFilesZip();
+			File dir = new File(ZIP_DIR);
+			file = new File(dir, ZIP_NAME);
+			InputStream is = new FileInputStream(file);
+			upload.exec(DB, SCHEMA, is, dir, conn);
+		} catch(Exception exp) {
+			throw new RuntimeException(exp);
+		} finally {
+			Database.close(conn);
+			file.delete();
+		}
 	}
 	
 	public static void uploadDatatables() {
@@ -14,4 +60,23 @@ public class UploadCsvForSqlServer {
 		// TODO
 	}
 	
+	private static void download() {
+		log.info("Looking for existing file");
+		File dir = new File(ZIP_DIR);
+		File out = new File(dir, ZIP_NAME);
+		if(dir.exists() == false && DOWNLOAD == true) {
+			FileUtil.mkdirs(dir);
+			log.info("Doing download");
+			log.info("DOWNLOAD: " + DOWNLOAD);
+			log.info("DIR: " + FileUtil.getCanonicalPath(dir));
+			HttpRequestClient client = new HttpRequestClient(URL);
+			client.doGet();
+			FileUtil.write(client.getResponseInputStream(), out);
+		} else {
+			log.info("Skipping download");
+			log.info("DOWNLOAD: " + DOWNLOAD);
+			log.info("DIR: " + FileUtil.getCanonicalPath(dir));
+		}
+	}
+
 }
