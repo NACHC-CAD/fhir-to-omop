34f70063f6a71a62d634e920245056827be0d130
greshje <greshje@gmail.com>
2024-06-23 05:05:15 -0400

    added add/delete webapi records

--- src/main/resources/postgres/build/A08_CreateAtlasSourceRecordsInWebApi.sql
+++ src/main/resources/postgres/build/A08_CreateAtlasSourceRecordsInWebApi.sql
@@ -1,7 +1,6 @@
 INSERT INTO webapi.source (source_id, source_name, source_key, source_connection, source_dialect) 
 SELECT nextval('webapi.source_sequence'), '<atlasDataSourceName>', '<atlasDataSourceKey>', '<atlasCdmUrl>', 'postgresql';
 
--- CHECK THIS LINE IF THERE IS AN ERROR (USE synthea_micro instead of synthea_micro.dbo)
 INSERT INTO webapi.source_daimon (source_daimon_id, source_id, daimon_type, table_qualifier, priority) 
 SELECT nextval('webapi.source_daimon_sequence'), source_id, 0, '<atlasCdm>', 0
 FROM webapi.source
@@ -9,7 +8,7 @@ WHERE source_key = '<atlasDataSourceKey>'
 ;
 
 INSERT INTO webapi.source_daimon (source_daimon_id, source_id, daimon_type, table_qualifier, priority) 
-SELECT nextval('webapi.source_daimon_sequence'), source_id, 1, '<atlasCdm>', 1
+SELECT nextval('webapi.source_daimon_sequence'), source_id, 1, '<atlasCdm>', 10
 FROM webapi.source
 WHERE source_key = '<atlasDataSourceKey>'
 ;
